0c69a1b87f6373e9e5f0d6bd1558528788c57eca
Lucas Lauro <lucaskns1996@gmail.com>
2022-04-09 14:36:36 -0300

    Modulos Star Wars, CEP, Github

---
+++ src/routes/endpoints.ts
@@ -0,0 +1,21 @@
+import Home from '../containers/home';
+import { RouteProps } from 'react-router-dom';
+import Combustivel from "../containers/combustivel";
+import StarWars from '../containers/star-wars';
+import StarWarsDetails from "../containers/star-wars-details";
+import Register from '../containers/register';
+
+const publicUrl = process.env.PUBLIC_URL;
+
+interface EndPointsProps extends RouteProps {
+  name?: string
+}
+
+export const endpoints: EndPointsProps[] = [
+  { path: `${publicUrl}/`, component: Home, exact: true },
+  { path: `${publicUrl}/home`, name: 'Home', component: Home, exact: true },
+  { path: `${publicUrl}/combustivel`, name: 'Combustível', component: Combustivel, exact: true },
+  { path: `${publicUrl}/star-wars/:id`, component: StarWarsDetails },
+  { path: `${publicUrl}/star-wars/`, name: 'Star Wars', component: StarWars, exact: true },
+  { path: `${publicUrl}/register/`, name: 'Register', component: Register, exact: true },
+];
